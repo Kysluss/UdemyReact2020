49c0e84fd607088d743316a9a33f22e19a22e2b3
Kyle Slusser <SLUKY@intlogistics.com>
2020-01-19 17:28:47 -0500

    Added semantic UI and stubbed out comment

--- components/src/index.js
+++ components/src/index.js
@@ -4,10 +4,24 @@ import ReactDOM from 'react-dom';
 
 // Create a react component
 const App = () => {
-    const buttonText = { text: 'Click me' };
-    const style = { backgroundColor: 'blue', color: 'white'};
-
-    return <div>Hi there</div>;
+    return (
+        <div className="ui container comments">
+            <div className="comment">
+                <a href="/" className="avatar">
+                    <img alt="Avatar" />
+                </a>
+                <div className="content">
+                    <a href="/" className="author">
+                        Sam
+                    </a>
+                    <div className="metadata">
+                        <span className="date">Today at 6:00PM</span>
+                    </div>
+                    <div className="text">Nice blog post</div>
+                </div>
+            </div>
+        </div>
+    );
 };
 
 // Take the react component and show it on the screen
