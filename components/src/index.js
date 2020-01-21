cfaaacd5ff637a62d49afee964617dc910371d1b
Kyle Slusser <SLUKY@intlogistics.com>
2020-01-20 19:28:01 -0500

    Created ApprovalCard component stubbed out

--- components/src/index.js
+++ components/src/index.js
@@ -2,12 +2,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import faker from 'faker';
-import CommentDetail from './CommentDetail'
+import CommentDetail from './CommentDetail';
+import ApprovalCard from './ApprovalCard';
 
 // Create a react component
 const App = () => {
     return (
         <div className="ui container comments">
+            <ApprovalCard />
             <CommentDetail author="Sam" timeAgo="Today at 6:00PM" content="Nice blog post!" avatar={faker.image.avatar()} />
             <CommentDetail author="Alex" timeAgo="Today at 2:00PM" content="I like the subject" avatar={faker.image.avatar()} />
             <CommentDetail author="Jane" timeAgo="Yesterday at 5:00PM" content="I like the writing" avatar={faker.image.avatar()} />
